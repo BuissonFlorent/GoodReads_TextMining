9feeb6f323185ea10703c6ca25f2505dc5c96f5a
Florent Buisson <florent.buisson@gmail.com>
2016-09-04 14:16:46 -0500

    Edited everything to conform to Google's R style guide.

--- GR_MachineLearning.R
+++ GR_MachineLearning.R
@@ -13,95 +13,107 @@ library(ROCR)
 ##### General Setup #####
 
 setwd("C:/Users/Florent/Desktop/Data_analysis_applications/GoodReads_TextMining")
-data=read.csv("GoodReadsCleanData.csv", stringsAsFactors = FALSE)
+data <- read.csv("GoodReadsCleanData.csv", stringsAsFactors = FALSE)
 
 set.seed(1234)
 # Creating the binary outcome feature
-data$good_read=0
-data$good_read[data$rating==4|data$rating==5]=1
+data$good.read <- 0
+data$good.read[data$rating == 4 | data$rating == 5] <- 1
 
-trainIdx <- createDataPartition(data$good_read, p = .75,
-                                  list = FALSE,
-                                  times = 1)
-train=data[trainIdx,]
-test=data.table(data[-trainIdx,])
+trainIdx <- createDataPartition(data$good.read, 
+                                p = .75,
+                                list = FALSE,
+                                times = 1)
+train <- data[trainIdx, ]
+test <- data.table(data[-trainIdx, ])
 
 
 #### Creating the Document-Term Matrix ####
 
 # Creating a DTM for the negative reviews
-sparsity=.99
-bad_dtm = create_matrix(train$review[train$good_read==0], language="english", 
-                    removeStopwords=FALSE, removeNumbers=TRUE, 
-                    stemWords=FALSE, removeSparseTerms = sparsity) 
+sparsity <- .99
+bad.dtm <- create_matrix(train$review[train$good.read == 0], 
+                         language = "english",
+                         removeStopwords = FALSE, 
+                         removeNumbers = TRUE,
+                         stemWords = FALSE, 
+                         removeSparseTerms = sparsity) 
 #Converting the DTM in a data frame
-#bad_dtm_df = data.table(as.matrix(bad_dtm),row.names = train$review_id[train$good_read==0])
-bad_dtm_df = data.table(as.matrix(bad_dtm))
+bad.dtm.df  <-  data.table(as.matrix(bad.dtm))
 # Creating a DTM for the positive reviews
-good_dtm = create_matrix(train$review[train$good_read==1], language="english", 
-                         removeStopwords=FALSE, removeNumbers=TRUE, 
-                         stemWords=FALSE, removeSparseTerms = sparsity) 
-#good_dtm_df = data.table(as.matrix(good_dtm),row.names = train$review_id[train$good_read==1])
-good_dtm_df = data.table(as.matrix(good_dtm))
+good.dtm <- create_matrix(train$review[train$good.read == 1], 
+                          language="english",
+                          removeStopwords = FALSE, 
+                          removeNumbers = TRUE,
+                          stemWords = FALSE, 
+                          removeSparseTerms = sparsity) 
+good.dtm.df <- data.table(as.matrix(good.dtm))
 # Joining the two DTM together
-train_dtm_df = bind_rows(bad_dtm_df,good_dtm_df)
-train_dtm_df$review_id = c(train$review_id[train$good_read==0],train$review_id[train$good_read==1])
-train_dtm_df = arrange(train_dtm_df, review_id)
-train_dtm_df$good_read=train$good_read
+train.dtm.df <- bind_rows(bad.dtm.df, good.dtm.df)
+train.dtm.df$review.id <- c(train$review.id[train$good.read == 0],
+                            train$review.id[train$good.read == 1])
+train.dtm.df <- arrange(train.dtm.df, review.id)
+train.dtm.df$good.read  <- train$good.read
 
-train_dtm_df = train %>%
-  select(-c(book,rating,review,good_read)) %>%
-  inner_join(train_dtm_df, by = "review_id") %>%
-  select(-review_id)
+train.dtm.df <- train %>%
+  select(-c(book, rating, review, good.read)) %>%
+  inner_join(train.dtm.df, by = "review.id") %>%
+  select(-review.id)
 
-train_dtm_df[is.na(train_dtm_df)] = 0
+train.dtm.df[is.na(train.dtm.df)] <- 0
 
 
 # Creating the test DTM
-test_dtm = create_matrix(test$review, language="english", 
-                        removeStopwords=FALSE, removeNumbers=TRUE, 
-                        stemWords=FALSE, removeSparseTerms = sparsity) 
-test_dtm_df=data.table(as.matrix(test_dtm))
-test_dtm_df$review_id=test$review_id
-test_dtm_df$good_read=test$good_read
+test.dtm <- create_matrix(test$review, 
+                          language = "english", 
+                          removeStopwords = FALSE, 
+                          removeNumbers = TRUE, 
+                          stemWords = FALSE, 
+                          removeSparseTerms = sparsity) 
+test.dtm.df <- data.table(as.matrix(test.dtm))
+test.dtm.df$review.id <- test$review.id
+test.dtm.df$good.read <- test$good.read
 
-test_dtm_df = test %>%
-  select(-c(book,rating,review,good_read)) %>%
-  inner_join(test_dtm_df, by = "review_id") %>%
-  select(-review_id)
+test.dtm.df <- test %>%
+  select(-c(book, rating, review, good.read)) %>%
+  inner_join(test.dtm.df, by = "review.id") %>%
+  select(-review.id)
 
 
 # Ensuring that the test DTM has the same columns as the train dataset
-test_dtm_df = head(bind_rows(test_dtm_df, train_dtm_df[1,]),-1)
-test_dtm_df = test_dtm_df %>% 
-  select(one_of(colnames(train_dtm_df)))
-test_dtm_df[is.na(test_dtm_df)] = 0
-test_dtm_df=data.table(test_dtm_df)
+test.dtm.df <- head(bind_rows(test.dtm.df, train.dtm.df[1, ]), -1)
+test.dtm.df <- test.dtm.df %>% 
+  select(one_of(colnames(train.dtm.df)))
+test.dtm.df[is.na(test.dtm.df)] <- 0
+test.dtm.df <- data.table(test.dtm.df)
 
 
 ##### Machine learning algorithm: XGBoost #####
 
-baseline_acc=sum(test$good_read=="1")/nrow(test)
+baseline.acc <- sum(test$good.read == "1") / nrow(test)
 
-XGB_train=as.matrix(select(train_dtm_df,-good_read),
-                    dimnames=dimnames(train_dtm_df))
-XGB_test=as.matrix(select(test_dtm_df,-good_read),
-                   dimnames=dimnames(test_dtm_df))
-XGB_model <- xgboost(data=XGB_train, label=train_dtm_df$good_read, 
-                     nrounds=400, objective="binary:logistic")
+XGB.train <-  as.matrix(select(train.dtm.df, -good.read),
+                        dimnames = dimnames(train.dtm.df))
+XGB.test <- as.matrix(select(test.dtm.df, -good.read),
+                      dimnames = dimnames(test.dtm.df))
+XGB.model <- xgboost(data = XGB.train, 
+                     label = train.dtm.df$good.read, 
+                     nrounds = 400, 
+                     objective = "binary:logistic")
 
-XGB_predict = predict(XGB_model,XGB_test)
+XGB.predict <- predict(XGB.model, XGB.test)
 
-XGB_results=data.frame(good_read=test$good_read,pred=XGB_predict)
+XGB.results <- data.frame(good.read = test$good.read, pred = XGB.predict)
 
-ROCR_pred <- prediction(XGB_results$pred,XGB_results$good_read)
-ROCR_perf <- performance(ROCR_pred, 'tnr','fnr') 
-plot(ROCR_perf, colorize = TRUE)
-XGB_table=table(true=XGB_results$good_read, pred=as.integer(XGB_results$pred >=0.80))
-XGB_table
-XGB_acc=sum(diag(XGB_table))/nrow(test)
+ROCR.pred <- prediction(XGB.results$pred, XGB.results$good.read)
+ROCR.perf <- performance(ROCR.pred, 'tnr','fnr') 
+plot(ROCR.perf, colorize = TRUE)
+XGB.table  <- table(true = XGB.results$good.read, 
+                    pred = as.integer(XGB.results$pred >= 0.80))
+XGB.table
+XGB.acc <- sum(diag(XGB.table)) / nrow(test)
 
 ### Feature analysis with XGBoost
-names <- colnames(test_dtm_df)
-importance_matrix <- xgb.importance(names, model = XGB_model)
-xgb.plot.importance(importance_matrix[1:20,])
+names <- colnames(test.dtm.df)
+importance.matrix <- xgb.importance(names, model = XGB.model)
+xgb.plot.importance(importance.matrix[1:20, ])
